d01928c170df667fcbc24e5bec8d95b77c116062
신동욱 <140220959+SHIN-DONG-UK@users.noreply.github.com>
2024-10-07 17:20:02 +0900

    Title: 조삼모사, Time: 304ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/조삼모사/three-at-dawn-and-four-at-dusk.cpp
+++ 241007/조삼모사/three-at-dawn-and-four-at-dusk.cpp
@@ -5,14 +5,11 @@ using namespace std;
 
 int N;
 int P[20][20];
-vector<int> v1;
-vector<int> v2;
-int visited[20];
+bool visited[20];
 
 void input();
 void go(int idx, int);
-int calc1();
-int calc2();
+int calc();
 int ans = 1e9;
 
 int main() {
@@ -35,14 +32,8 @@ void input() {
 
 void go(int idx, int start) {
   if (idx == N/2) {
-    for (int i = 0; i < N; i++){
-      if (visited[i] == 1) continue;
-      v2.push_back(i);
-    }
-    int sum1 = calc1();
-    int sum2 = calc2();
-    if (ans > abs(sum1 - sum2)) ans = abs(sum1 - sum2);
-    v2.clear();
+    int diff = calc();
+    if (ans > diff) ans = diff;
     return;
   }
 
@@ -50,40 +41,25 @@ void go(int idx, int start) {
   {
     if (visited[i] == 1) continue;
     visited[i] = 1;
-    v1.push_back(i);
     go(idx + 1, i + 1);
     visited[i] = 0;
-    v1.pop_back();
   }
 
 }
 
-int calc1() {
-  int rst = 0;
-
-  for (int i = 0; i < N/2; i++)
+int calc() {
+  int morningSum = 0; int eveningSum = 0;
+  for (int i = 0; i < N; i++)
   {
-    for (int j = 0; j < N/2; j++)
+    for (int j = 0; j < N; j++)
     {
       if (i == j) continue;
-      rst += P[v1[i]][v1[j]];
+      if (visited[i] == 1 && visited[j] == 1)
+        morningSum += P[i][j];
+      if (visited[i] == 0 && visited[j] == 0)
+        eveningSum += P[i][j];
     }
   }
 
-  return rst;
-}
-
-int calc2() {
-  int rst = 0;
-
-  for (int i = 0; i < N / 2; i++)
-  {
-    for (int j = 0; j < N / 2; j++)
-    {
-      if (i == j) continue;
-      rst += P[v2[i]][v2[j]];
-    }
-  }
-
-  return rst;
+  return abs(morningSum - eveningSum);
 }
