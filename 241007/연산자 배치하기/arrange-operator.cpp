100b572ff1cb45cae37d2473c621c37c1b1a53c1
신동욱 <140220959+SHIN-DONG-UK@users.noreply.github.com>
2024-10-07 19:14:02 +0900

    Title: 연산자 배치하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241007/연산자 배치하기/arrange-operator.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+int arr[11];
+vector<char> operators;
+int visited[10];
+int path[10];
+int sum;
+int min_val = 1e9;
+int max_val = 0;
+
+void input();
+void go(int idx);
+
+int main() {
+    input();
+    go(0);
+    cout << min_val << ' ' << max_val << '\n';
+}
+
+void input() {
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    int a, b, c;
+    cin >> a >> b >> c;
+    for (int i = 0; i < a; i++)
+        operators.push_back('+');
+    for (int i = 0; i < b; i++)
+        operators.push_back('-');
+    for (int i = 0; i < c; i++)
+        operators.push_back('*');
+}
+
+void go(int idx) {
+    if (idx == N - 1) {
+        sum = arr[0];
+        for (int i = 0; i < N-1; i++)
+        {
+            if (operators[path[i]] == '+') {
+                sum += arr[i + 1];
+            }
+            else if (operators[path[i]] == '-') {
+                sum -= arr[i + 1];
+            }
+            else {
+                sum *= arr[i + 1];
+            }
+        }
+        if (min_val > sum) min_val = sum;
+        if (sum > max_val) max_val = sum;
+        return;
+    }
+    for (int i = 0; i < N - 1; i++) {
+        if (visited[i] == 1) continue;
+        visited[i] = 1;
+        path[idx] = i;
+        go(idx + 1);
+        visited[i] = 0;
+    }
+}
