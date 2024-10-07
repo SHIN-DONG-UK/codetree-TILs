fce8fca7100dbef616b7c659ae20ffbe634e5d82
신동욱 <140220959+SHIN-DONG-UK@users.noreply.github.com>
2024-10-07 15:10:12 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241007/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct Task {
+  int t;
+  int p;
+};
+
+int N;
+Task tasks[15];
+int ans;
+
+void input();
+void go(int idx, int sum);
+
+int main() {
+  input();
+  go(0, 0);
+
+  cout << ans << '\n';
+}
+
+void input() {
+  cin >> N;
+  for (int i = 0; i < N; i++)
+  {
+    cin >> tasks[i].t;
+    cin >> tasks[i].p;
+  }
+}
+
+void go(int idx, int sum) {
+  if (idx >= N) {
+    if (sum > ans) ans = sum;
+    return;
+  }
+
+  go(idx + 1, sum);
+  go(idx + tasks[idx].t, sum + tasks[idx].p);
+
+}
